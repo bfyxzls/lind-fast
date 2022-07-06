6df320b7ab32d920de75fad33008fd28d6fd8610
zhangzhanling <bfyxzls@sina.com>
2022-07-06 17:54:33 +0800

    update pom

--- lind-gateway/src/main/java/com/lind/gateway/LindGatewayApplication.java
+++ lind-gateway/src/main/java/com/lind/gateway/LindGatewayApplication.java
@@ -1,6 +1,5 @@
 package com.lind.gateway;
 
-import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,7 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@GlobalTransactional // 分布式事务注解
 public class LindGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(LindGatewayApplication.class, args);
