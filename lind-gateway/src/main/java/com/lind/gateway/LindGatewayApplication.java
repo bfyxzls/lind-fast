f1169f1ae9d9596feea3eaa90715eb71b32d55c6
zhangzhanling <bfyxzls@sina.com>
2022-07-06 17:29:22 +0800

    add workflow

--- lind-gateway/src/main/java/com/lind/gateway/LindGatewayApplication.java
+++ lind-gateway/src/main/java/com/lind/gateway/LindGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.lind.gateway;
 
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@GlobalTransactional // 分布式事务注解
 public class LindGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(LindGatewayApplication.class, args);
